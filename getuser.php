9cdf9600a31483691531120399638df425462c8e
atpluem <45143217+atpluem@users.noreply.github.com>
2020-04-29 22:00:14 +0700

    Update getuser.php

--- getuser.php
+++ getuser.php
@@ -4,7 +4,7 @@
 $postdata = file_get_contents("php://input");
 $personalid=$_GET["username"];
  
-$sql="SELECT * FROM councilmember where PersonalID='6107050000001'";
+$sql="SELECT * FROM councilmember where PersonalID='$personalid'";
  
 if($result = mysqli_query($mysqli,$sql))
 {
