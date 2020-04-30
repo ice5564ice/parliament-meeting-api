7f62ceff1daa1609db3d71d634f520451c8bbf3a
xzozero5 <63011472+xzozero5@users.noreply.github.com>
2020-04-30 22:13:11 +0700

    AVG time of each conferencetype

---
+++ Analysis_Report/Query02.php
@@ -0,0 +1,27 @@
+<?php
+  include_once("config.php");
+ 
+$postdata = file_get_contents("php://input");
+$personalid=$_GET["username"];
+ 
+$sql="SELECT t.ConferenceTypeName,TIME(AVG(c.EndTime - c.StartTime)) AS AverageTime
+      FROM councilconference c,conferencetype t
+      WHERE c.conferencetypeID = t.conferencetypeID
+      GROUP BY  t.conferencetypeID
+      ORDER BY AverageTime;";
+ 
+if($result = mysqli_query($mysqli,$sql))
+{
+ $rows = array();
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $rows[] = $row;
+  }
+ 
+  echo json_encode($rows);
+}
+else
+{
+  http_response_code(404);
+}
+?>
