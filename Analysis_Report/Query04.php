7d8c32d61c651649db07b2d6adda556e09ea6165
xzozero5 <63011472+xzozero5@users.noreply.github.com>
2020-04-30 22:18:16 +0700

    ์ีNumber of Members of each Party

---
+++ Analysis_Report/Query04.php
@@ -0,0 +1,29 @@
+<?php
+  include_once("config.php");
+ 
+$postdata = file_get_contents("php://input");
+$personalid=$_GET["username"];
+ 
+$sql="SELECT p.PartyName,COUNT(*) AS members,CONCAT(FORMAT((COUNT(*)*100 / 
+            (SELECT count( * ) 
+            FROM councilmember 
+            WHERE PartyName IS NOT NULL)),2) ,  "%") AS percentage
+      FROM politicalparty p,councilmember m
+      GROUP BY m.PartyName = p.partyName
+      ORDER BY members;";
+ 
+if($result = mysqli_query($mysqli,$sql))
+{
+ $rows = array();
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $rows[] = $row;
+  }
+ 
+  echo json_encode($rows);
+}
+else
+{
+  http_response_code(404);
+}
+?>
